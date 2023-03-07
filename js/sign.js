f00229d4a179f3e5f5c55d64f2d4704bebac741e
DUNIEN811 <dunien811@email.com>
2023-03-07 14:49:55 +0700

    NIEN

--- js/sign.js
+++ js/sign.js
@@ -1,41 +1,11 @@
-$('#quenmk').click(function () {
-    // e.preventDefault();
-    $('#a-sign').css(
-        "display", "none"
-    )
-    $('#b-sign').css(
-        "display", "block"
-    )
-    console.log('a')
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
 
-});
-$('#huy').click(function () {
-    // e.preventDefault();
-    $('#b-sign').css(
-        "display", "none"
-    )
-    $('#a-sign').css(
-        "display", "block"
-    )
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
 });
 
-$('#dangnhap').click(function (e) {
-    e.preventDefault();
-
-    const email = document.getElementById('username');
-    const password = $('#password').val();
-    const filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-    if (!filter.test(email.value)) {
-        alert('Hay nhap dia chi email hop le.\nExample@gmail.com');
-        email.focus;
-        return false;
-    }
-    if (password === '') {
-        $('#password').html(`<div class="error" style="position: absolute; 
-        bottom: 0;background: #fff; padding:10px; border:1px solid #ccc; color: red">Please fill out this field </div>`)
-        alert('Hay nhap password');
-
-    }
-
-
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
 });
